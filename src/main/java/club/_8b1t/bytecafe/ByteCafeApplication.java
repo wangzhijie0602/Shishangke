30febd369f65457f792f950b6b7ac47339f5e2df
wangzhijie0602 <46142484+wangzhijie0602@users.noreply.github.com>
2024-12-09 20:00:15 +0800

    feat:项目诞生

---
+++ src/main/java/club/_8b1t/bytecafe/ByteCafeApplication.java
@@ -0,0 +1,13 @@
+package club._8b1t.bytecafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/_8b1t/bytecafe/ByteCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package club._8b1t.bytecafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
