7cfb9b8c3fafd446c941965dbe5089044ec7c091
wangzhijie0602 <46142484+wangzhijie0602@users.noreply.github.com>
2025-02-27 16:00:56 +0800

    引入Mybatis-plus-join和分页扩展

---
+++ src/main/java/club/_8b1t/config/MybatisPlusConfig.java
@@ -0,0 +1,27 @@
+package club._8b1t.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 8bit
+ */
+@Configuration
+@MapperScan("club._8b1t.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 如果有多数据源可以不配具体类型, 否则都建议配上具体的 DbType
+        return interceptor;
+    }
+}
--- src/main/java/club/_8b1t/config/SaTokenConfigure.java
+++ src/main/java/club/_8b1t/config/SaTokenConfigure.java
@@ -1,19 +1,25 @@
-package club._8b1t.config;
+package com.bit.config;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
+import cn.dev33.satoken.router.SaHttpMethod;
+import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author 8bit
+ */
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
     // 注册拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
-                .addPathPatterns("/**")
-                .excludePathPatterns("/user/login", "/user/register");
+        // 注册 Sa-Token 拦截器
+        registry.addInterceptor(new SaInterceptor(handler -> {
+//            排除所有的预检请求
+            SaRouter.notMatch(SaHttpMethod.OPTIONS).check(r -> StpUtil.checkLogin());
+        })).addPathPatterns("/**").excludePathPatterns("/**/login", "/**/register", "/swagger-ui/**", "/v3/api-docs");
     }
 }
